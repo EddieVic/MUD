f3cde47d41769ada52c3978d4a9e0ef2aec9cd39
stephenramirez <stephenramirez@email.arizona.edu>
2016-10-12 13:34:07 -0700

    fixed java project problem

---
+++ src/MUDGui.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class MUDGui extends JFrame implements Observer{
+	
+	public MUDGui()
+	{
+		
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
